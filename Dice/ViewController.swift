0f4baa5426a9b34a44220bc09e4bf616333a9bb2
Anirudh V <anirudhv@anirudhs-macbook-pro.local>
2018-06-06 18:57:08 -0700

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Anirudh V on 6/6/18.
+//  Copyright © 2018 Anirudh V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
