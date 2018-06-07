c4ab9f14ad365d8699c0185e92366725667faaf6
Anirudh V <anirudhv@Anirudhs-MacBook-Pro.local>
2018-06-07 02:05:23 -0700

    initial

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    var randomindex1: Int = 0
+    var randomindex2: Int = 0
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var Tot: UILabel!
+    let dicearray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func rollButton(_ sender: Any) {
+        randomindex1 = Int(arc4random_uniform(6))
+        randomindex2 = Int(arc4random_uniform(6))
+        
+       diceImageView1.image = UIImage(named: dicearray[randomindex1])
+       diceImageView2.image = UIImage(named: dicearray[randomindex2])
+        let tot = randomindex2+1 + randomindex1+1
+        Tot.text = String(tot)
+    }
+    
 }
 
